42ff18d5e348d46ea044fdaef5f5b4d441235620
krishnajais001 <krishnajais7091@gmail.com>
2024-02-21 01:08:41 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Assignment 2
+let person = {};
+person.Name = "John";
+person.age = 30;
+person.city = "New York";
+delete person.age;
+person.Job = "Engineer";
+person.city = "San Frasisco";
+console.log(person);
